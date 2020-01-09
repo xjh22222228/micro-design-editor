66d747d7b676d34769acde0a62bd786f6c028b25
xjh22222228 <xjh22222228@gmail.com>
2020-01-09 09:06:14 +0800

    Updates

---
+++ precache-manifest.9024c3338566ec8dddb17695bebc155a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd73887228bd2111fd5e19095e14437d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3551ae0f5c15cb44cf78",
+    "url": "/static/css/2.9da66810.chunk.css"
+  },
+  {
+    "revision": "d8b1755a417157d7e572",
+    "url": "/static/css/main.ed22edfd.chunk.css"
+  },
+  {
+    "revision": "3551ae0f5c15cb44cf78",
+    "url": "/static/js/2.951e14c4.chunk.js"
+  },
+  {
+    "revision": "43937419b0c57d53f85a3eec27f86e5e",
+    "url": "/static/js/2.951e14c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8b1755a417157d7e572",
+    "url": "/static/js/main.e5f245de.chunk.js"
+  },
+  {
+    "revision": "b021d9a1da770ea02917",
+    "url": "/static/js/runtime-main.4acfb9c0.js"
+  }
+]);
